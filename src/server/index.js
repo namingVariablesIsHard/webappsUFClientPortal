d6cb461ac4150117035215b2ac9d7b061675a6b3
Ronald DeLuca <rdv1123@ufl.edu>
2018-11-17 15:42:02 -0500

    Package Update

--- src/server/index.js
+++ src/server/index.js
@@ -4,8 +4,9 @@ const app = express();
 const path = require('path');
 
 app.use(express.static('dist'));
+// app.use(express.static(__dirname + '/public'));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'), (err) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html' || 'index.html'), (err) => {
     if (err) {
       res.status(500).send(err);
     }
